2439b95c056afdb755c5d1dff1c155525a31787e
millapupo <millamekary@gmail.com>
2023-04-11 16:15:11 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Pizza basePizza = new Pizza(false);
+		basePizza.addExtraCheese();
+		basePizza.addExtraToppings();
+		basePizza.TakeAway();
+		basePizza.getBill();
+
+	}
+
+}
